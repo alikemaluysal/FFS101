59c853401d6345672fd7d7c4b33b4d3f86a538fb
Ali Kemal Uysal <alikemaluysal4235@gmail.com>
2023-12-15 22:08:28 +0300

    Add project files.

---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Entities;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private static readonly List<User> _users;
+    private static int _index;
+
+    static UsersController()
+    {
+        _users = new List<User>()
+        {
+            new User { Id = 1, Email = "emre@siliconmade.com", Username = "emre", Password = "1234"},
+            new User { Id = 2, Email = "kerem@siliconmade.com", Username = "kerem", Password = "kerem12"},
+            new User { Id = 3, Email = "seda@siliconmade.com", Username = "seda", Password = "password"},
+        };
+
+        _index = _users.Count + 1;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+
+        return Ok(_users);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get([FromRoute] int id)
+    {
+
+        var user = _users.FirstOrDefault(x => x.Id == id);
+
+        if (user is null)
+            return NotFound($"Kullanıcı bulunamadı!");
+
+        return Ok(user);
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] UserCreateModel model)
+    {
+
+        if(!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        //Manual Mapping -> Auto Mapping (AutoMapper)
+        User user = new User()
+        {
+            Id = _index++,
+            Email = model.Email,
+            Username = model.Username,
+            Password = model.Password
+        };
+        _users.Add(user);
+        return Ok(user);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete([FromRoute] int id)
+    {
+        var user = _users.FirstOrDefault(u => u.Id == id);
+        if (user is null)
+            return NotFound("Kullanıcı bulunmadı");
+
+        _users.Remove(user);
+        return Ok(user);
+
+    }
+
+
+}
---
+++ WebAPI/Models/UserCreateModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models;
+
+public class UserCreateModel
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [MinLength(3)]
+    [StringLength(50)]
+    public string Username { get; set; }
+    [Required]
+    [MinLength(4)]
+    [StringLength(10)]
+    public string Password { get; set; }
+
+    [Compare(nameof(Password))]
+    public string ConfirmPassword { get; set; }
+}
